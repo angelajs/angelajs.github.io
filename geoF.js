6b7cc9a9984a7049577ce98ccdcbc216c5ba280e
angelajs <angelajs17@gmail.com>
2017-03-10 11:16:32 +0000

    Update geoF.js

--- geoF.js
+++ geoF.js
@@ -1,31 +1,16 @@
 
-navigator.serviceWorker.register('serviceworker.js').then(registration => {
-  console.log("success!");
-  if (registration.installing) {
-    registration.installing.postMessage("Howdy from your installing page.");
-  }
-}, err => {
-  console.error("Installing the worker failed!", err);
-});
-///    );
-        //navigatior.serviceWorker.register('/service-worker.js');
-///}
-//     .register('serviceworker.js')
-//     .then((swRegistration) => {
-//         let region = new CircularGeofenceRegion({
-//             name: 'fence',
-//             latitude: 52.301764,
-//             longitude: -0.002352,
-//             radius: 10
-//         });
-//         let options = {
-//             includePositon: true
-//         };
-//         swRegistration.geofencing.add( region, options)
-//         .then(
-//             (geofence) => console.log(geofence.id),
-//             (error) => console.log(error)
-//         );
-//     });
-
-// };
+navigator.serviceWorker.register('serviceWorker.js').then((registration) => {
+  let region = new CircularGeofenceRegion({
+    name: 'fence',
+    latitude: 52.301764,
+    longitude: -0.002352,
+    radius: 10
+  });
+  let options = {
+    includePositon: true
+  };
+  registration.geofencing.add(region, options).then(
+    (geofence) => console.log(geofence.id),
+    (error) => console.log(error)
+    );
+  });
