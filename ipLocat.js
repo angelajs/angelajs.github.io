8633ddc93b46e130df34bde80350114bf44e4041
angelajs <angelajs17@gmail.com>
2017-03-29 10:56:59 +0100

    Update ipLocat.js

--- ipLocat.js
+++ ipLocat.js
@@ -1,12 +1,6 @@
-var xhr = new XMLHttpRequest();
-xhr.open('GET', "//api.ipinfodb.com/v3/ip-country/?key=<f31438f44d02b1cb4c9318bbe967419ca9ba34b9150499b63ab47d51849fd4be>&format=json", true);
-xhr.send();
+$.getJSON("http://api.ipinfodb.com/v3/ip-country/?key=a7593aaff69055862efe426544a894d7efb53491f4e82e5f3a8f6d99ca30cd4b&format=json&callback=?", jData);
 
-xhr.onreadystatechange = processRequest;
 
-function processRequest(e){
-    if (xhr.readyState == 4 && xhr.status == 200){
-        var response = JSON.parser(xhr.responseText);
-        alert(response.ip);
-    }
+function jData(json) {
+    alert("Value of ipAddress: " + json["ipAddress"] + json["countryCode"]);
 }
