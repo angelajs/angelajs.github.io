80c3429f59cdd620caa7bea48a82249b42a81370
angelajs <angelajs17@gmail.com>
2017-03-29 12:45:32 +0100

    Update ipLocat.js

--- ipLocat.js
+++ ipLocat.js
@@ -1,6 +1,18 @@
 $.getJSON("//api.ipinfodb.com/v3/ip-country/?key=a7593aaff69055862efe426544a894d7efb53491f4e82e5f3a8f6d99ca30cd4b&format=json&callback=?", location);
 
+var approved = [
+    //EEA
+    'AT', 'BE', 'BG', 'HR', 'CY', 'CZ', 'DK', 'EE', 'FI', 'FR', 'DE', 'GR', 'HU', 'IS', 'IE', 'IT', 'LV', 'LI', 'LT', 'LU', 'MT', 'NI', 'NO', 'PL', 'PT', 'RO', 'SK', 'SI', 'ES', 'SE', 'GB', 
+    //NON EEA
+    'AD', 'AR', 'CA', 'FO', 'GG', 'IM', 'IL', 'JE', 'NZ', 'CH', 'UY'
+];
 
-function location(json) {
-    alert("Value of ipAddress: " + json["ipAddress"] + " and " + json["countryCode"]);
+function location (json) {
+    var currentLocation = $.inArray (json["countryCode"], approved);
+
+    if (currentLocation > -1){
+        alert ("IN");
+    } else {
+        alert ("OUT. Location: " + json["countryCode"]);
+    }
 }
