6b6302caf215bc71b8ffc4505f08c576784b33a0
angelajs <angelajs17@gmail.com>
2017-03-29 11:13:17 +0100

    Update ipLocat.js

--- ipLocat.js
+++ ipLocat.js
@@ -1,4 +1,4 @@
-$.getJSON("http://api.ipinfodb.com/v3/ip-country/?key=a7593aaff69055862efe426544a894d7efb53491f4e82e5f3a8f6d99ca30cd4b&format=json&callback=?", jData);
+$.getJSON("https://api.ipinfodb.com/v3/ip-country/?key=a7593aaff69055862efe426544a894d7efb53491f4e82e5f3a8f6d99ca30cd4b&format=json&callback=?", jData);
 
 
 function jData(json) {
