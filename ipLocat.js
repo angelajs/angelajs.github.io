053f66896bd85eb3b77ee248369d10fd424197c3
angelajs <angelajs17@gmail.com>
2017-03-31 09:06:17 +0100

    Update ipLocat.js

--- ipLocat.js
+++ ipLocat.js
@@ -1,6 +1,7 @@
 $.getJSON("//api.ipinfodb.com/v3/ip-country/?key=a7593aaff69055862efe426544a894d7efb53491f4e82e5f3a8f6d99ca30cd4b&format=json&callback=?", location);
 
 var approved = [
+    //Country code from: https://www.iso.org/obp/ui/#search
     //EEA
     'AT', 'BE', 'BG', 'HR', 'CY', 'CZ', 'DK', 'EE', 'FI', 'FR', 'DE', 'GR', 'HU', 'IS', 'IE', 'IT', 'LV', 'LI', 'LT', 'LU', 'MT', 'NI', 'NO', 'PL', 'PT', 'RO', 'SK', 'SI', 'ES', 'SE', 'GB', 
     //NON EEA
